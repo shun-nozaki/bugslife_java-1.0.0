67ac401695cd638df1209e3b90269170c47324ed
野﨑駿 <shun.nozaki@fox-hound.jp>
2023-09-17 17:27:35 +0900

    fix: 商品一覧の検索方式を修正

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -17,6 +17,7 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Predicate;
 
 import com.example.entity.ProductWithCategoryName;
 import com.example.form.ProductForm;
@@ -77,48 +78,50 @@ public class ProductService {
 			categoryJoin.get("name").alias("categoryName")
 		).where(builder.equal(root.get("shopId"), shopId));
 
+		Predicate finalPredicate = builder.equal(root.get("shopId"), shopId);
 		// formの値を元に検索条件を設定する
 		if (!StringUtils.isEmpty(form.getName())) {
 			// name で部分一致検索
-			query.where(builder.like(root.get("name"), "%" + form.getName() + "%"));
+			finalPredicate = builder.and(finalPredicate, builder.like(root.get("name"), "%" + form.getName() + "%"));
 		}
 
 		if (!StringUtils.isEmpty(form.getCode())) {
 			// code で部分一致検索
-			query.where(builder.like(root.get("code"), "%" + form.getCode() + "%"));
+			finalPredicate = builder.and(finalPredicate, builder.like(root.get("code"), "%" + form.getCode() + "%"));
 		}
 
 		if (form.getCategories() != null && form.getCategories().size() > 0) {
 			// categories で完全一致検索
-			query.where(categoryJoin.get("id").in(form.getCategories()));
+			finalPredicate = builder.and(finalPredicate, categoryJoin.get("id").in(form.getCategories()));
 		}
 
 		// weight で範囲検索
 		if (form.getWeight1() != null && form.getWeight2() != null) {
-			query.where(builder.between(root.get("weight"), form.getWeight1(), form.getWeight2()));
+			finalPredicate = builder.and(finalPredicate, builder.between(root.get("weight"), form.getWeight1(), form.getWeight2()));
 		} else if (form.getWeight1() != null) {
-			query.where(builder.greaterThanOrEqualTo(root.get("weight"), form.getWeight1()));
+			finalPredicate = builder.and(finalPredicate, builder.greaterThanOrEqualTo(root.get("weight"), form.getWeight1()));
 		} else if (form.getWeight2() != null) {
-			query.where(builder.lessThanOrEqualTo(root.get("weight"), form.getWeight2()));
+			finalPredicate = builder.and(finalPredicate, builder.lessThanOrEqualTo(root.get("weight"), form.getWeight2()));
 		}
 
 		// height で範囲検索
 		if (form.getHeight1() != null && form.getHeight2() != null) {
-			query.where(builder.between(root.get("height"), form.getHeight1(), form.getHeight2()));
+			finalPredicate = builder.and(finalPredicate, builder.between(root.get("height"), form.getHeight1(), form.getHeight2()));
 		} else if (form.getHeight1() != null) {
-			query.where(builder.greaterThanOrEqualTo(root.get("height"), form.getHeight1()));
+			finalPredicate = builder.and(finalPredicate, builder.greaterThanOrEqualTo(root.get("height"), form.getHeight1()));
 		} else if (form.getHeight2() != null) {
-			query.where(builder.lessThanOrEqualTo(root.get("height"), form.getHeight2()));
+			finalPredicate = builder.and(finalPredicate, builder.lessThanOrEqualTo(root.get("height"), form.getHeight2()));
 		}
 
 		// price で範囲検索
 		if (form.getPrice1() != null && form.getPrice2() != null) {
-			query.where(builder.between(root.get("price"), form.getPrice1(), form.getPrice2()));
+			finalPredicate = builder.and(finalPredicate, builder.between(root.get("price"), form.getPrice1(), form.getPrice2()));
 		} else if (form.getPrice1() != null) {
-			query.where(builder.greaterThanOrEqualTo(root.get("price"), form.getPrice1()));
+			finalPredicate = builder.and(finalPredicate, builder.greaterThanOrEqualTo(root.get("price"), form.getPrice1()));
 		} else if (form.getPrice2() != null) {
-			query.where(builder.lessThanOrEqualTo(root.get("price"), form.getPrice2()));
+			finalPredicate = builder.and(finalPredicate, builder.lessThanOrEqualTo(root.get("price"), form.getPrice2()));
 		}
+		query.where(finalPredicate);
 
 		return entityManager.createQuery(query).getResultList();
 	}
